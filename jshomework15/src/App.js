35213a353b858425785b6ed9635354e26e0fd8c8
AnnaShunkar <anna.shunkar4@gmail.com>
2026-02-03 16:39:25 +0200

    initial commit

--- jshomework15/src/App.js
+++ jshomework15/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import UseEffectComponent from './UseEffectComponent';
-import UseLayoutEffectComponent from './UseLayoutEffectComponent';
-import UseStateComponent from "./UseStateComponent";
-import UseReducerComponent from './UseReducerComponent';
-import UseRefComponent from './UseRefComponent';
+// import UseEffectComponent from './UseEffectComponent';
+// import UseLayoutEffectComponent from './UseLayoutEffectComponent';
+// import UseStateComponent from "./UseStateComponent";
+// import UseReducerComponent from './UseReducerComponent';
+// import UseRefComponent from './UseRefComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <UseEffectComponent />
+        {/* <UseEffectComponent />
         <UseLayoutEffectComponent />
         <UseStateComponent />
         <UseReducerComponent/>
-        <UseRefComponent/>
+        <UseRefComponent/> */}
       </header>
     </div>
   );
--- jshomework15/src/UseRefComponent.js
+++ jshomework15/src/UseRefComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 import { useRef } from "react";
 
 // Створіть компонент, який використовує хук useRef.
